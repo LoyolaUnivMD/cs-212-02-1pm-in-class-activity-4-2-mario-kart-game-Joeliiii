e11f2b45682515471c10859e4a828a78440cd46e
Joeliii <joelboy12704@gmail.com>
2024-02-08 16:58:37 -0500

    Code and Algorithm (idk how test cases apply to this)

--- marioKart.java
+++ marioKart.java
@@ -3,10 +3,39 @@
 // Your code here
 // Delete these 4 lines of comments
 
-class HelloWorld {
+class MarioKartGame {
     public static void main(String[] args) {
 
-        System.out.println("Mario Kart!");
+        MarioKartPlayer Joel = new MarioKartPlayer("Joel", 0, "Mario");
+        MarioKartPlayer Anneliese = new MarioKartPlayer("Anneliese", 10, "Rosalina");
+        Anneliese.displayinfo();
+        Joel.displayinfo();
+        Joel.boostSpeed();
+        Anneliese.boostSpeed();
+        Anneliese.displayinfo();
+        Joel.displayinfo();
+
         
     }
+}
+class MarioKartPlayer{
+    private String PlayerName;
+    private double speed;
+    private String character;
+
+    public MarioKartPlayer(String PlayerName, double speed, String character){
+        this.PlayerName = PlayerName;
+        this.character = character;
+        this.speed = speed;
+
+    }
+
+    public void boostSpeed(){
+        speed += 15;
+        System.out.println(PlayerName + "'s speed has increased.");
+    }
+    public void displayinfo(){
+        System.out.println(PlayerName +"'s character is " + character + " and their speed is " + speed);
+    }
+
 }
